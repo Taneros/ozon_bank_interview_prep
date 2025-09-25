9d7fcbb032ceedf91dac683d9365c7050b158f75
taneros <taneros@duck.com>
2025-09-25 23:20:54 +0300

    feat: add useUserFilters

---
+++ client/src/hooks/useUserFilters.ts
@@ -0,0 +1,25 @@
+import type { TUserFilters } from "@/hooks/useUsers";
+import { useMemo, useState } from "react";
+
+export const useUserFilters = () => {
+  const [filters, setFilters] = useState<TUserFilters>({});
+
+  const setGlobalSearch = (search: string) => {
+    setFilters((prev) => ({ ...prev, globalSearch: search || undefined }));
+  };
+
+  const clearAllFilters = () => {
+    setFilters({});
+  };
+
+  const hasActiveFilters = useMemo(() => {
+    return filters.globalSearch !== undefined && filters.globalSearch !== ''
+  }, [filters])
+
+  return {
+    filters,
+    setGlobalSearch,
+    clearAllFilters,
+    hasActiveFilters
+  }
+};
